863a7fffe5e3487c38210d00ab6dade768918527
Edrine Frances A. Esguerra <153322348+snsnzjkt@users.noreply.github.com>
2025-08-31 10:59:04 +0800

    Update deepseek_rl.py

--- models/deepseek_rl.py
+++ models/deepseek_rl.py
@@ -281,10 +281,10 @@ class DQNAgent:
             return
         
         batch = random.sample(self.memory, batch_size)
-        states = torch.FloatTensor([e[0] for e in batch]).to(self.device)
+        states = torch.FloatTensor(np.array([e[0] for e in batch])).to(self.device)
         actions = torch.LongTensor([e[1] for e in batch]).to(self.device)
         rewards = torch.FloatTensor([e[2] for e in batch]).to(self.device)
-        next_states = torch.FloatTensor([e[3] for e in batch]).to(self.device)
+        next_states = torch.FloatTensor(np.array([e[3] for e in batch])).to(self.device)
         dones = torch.BoolTensor([e[4] for e in batch]).to(self.device)
         
         current_q_values = self.q_network(states).gather(1, actions.unsqueeze(1))
