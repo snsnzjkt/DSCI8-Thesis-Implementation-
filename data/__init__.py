032145abac13f5e212c259fd9c434e5b2f7c91d5
snsnzjkt <edrineesguerra4@gmail.com>
2025-10-07 19:05:20 +0800

    data/__init__.py CODE FIX

--- data/__init__.py
+++ data/__init__.py
@@ -3,12 +3,6 @@ Data preprocessing and handling modules
 """
 
 from .preprocess import CICIDSPreprocessor
-
-try:
-    from .download_dataset import download_dataset
-except ImportError:
-    pass
-
 __all__ = [
     'CICIDSPreprocessor'
 ]
